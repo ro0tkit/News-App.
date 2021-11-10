850d554404a5072263b77f243cd10a7875e4ca5f
Luther <lutherking30@gmail.com>
2021-11-10 12:27:01 +0300

    Added blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,7 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
     # setting config
     from .request import configure_request
     configure_request(app)
